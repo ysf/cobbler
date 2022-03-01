0b9fc84cb1670dbaabe7cf648f642416df34594d
Enno Gotthold <egotthold@suse.de>
2022-03-01 14:56:59 +0100

    Tmp

--- tests/modules/managers/genders_test.py
+++ tests/modules/managers/genders_test.py
@@ -1,3 +1,4 @@
+from cobbler.templar import Templar
 from cobbler.modules.managers import genders
 
 
@@ -10,19 +11,30 @@ def test_register():
     assert result == "/var/lib/cobbler/triggers/change/*"
 
 
-def test_write_genders_file():
+def test_write_genders_file(mocker):
     # Arrange
+    templar_mock = mocker.patch(Templar, autospec=True)
+    # TODO: Create mock API
+    # TODO: Create mock mgmtclasses
+    # TODO: Create mock distros
+    # TODO: Create mock profiles
+
     # Act
-    result = genders.write_genders_file()
+    result = genders.write_genders_file(None, None, None, None)
 
     # Assert
-    assert False
+    assert templar_mock.render.call_count == 1
+    templar_mock.render.assert_called_with("")
 
 
 def test_run():
     # Arrange
+    # TODO: Create mock API
+    # TODO: Mock write_genders_file
+
     # Act
-    result = genders.run()
+    result = genders.run(None, [])
 
     # Assert
-    assert False
+    # TODO: Assert correct call to write_genders_file
+    assert result == 0
